c5648baf6adfce128e6eeb0a9940c90f92541bb3
ST <slokas85@gmail.com>
2022-08-05 23:44:09 +0300

    version 1

---
+++ HW_3M_1L/AnimationModel.swift
@@ -0,0 +1,38 @@
+//
+//  AnimationModel.swift
+//  HW_3M_1L
+//
+//  Created by Sergejs Tiselskis on 05/08/2022.
+//
+
+import Foundation
+
+struct Animation {
+
+    shake
+    pop
+    morph
+    squeeze
+    wobble
+    swing
+    flipX
+    flipY
+    fall
+    squeezeLeft
+    squeezeRight
+    squeezeDown
+    squeezeUp
+    slideLeft
+    slideRight
+    slideDown
+    slideUp
+    fadeIn
+    fadeOut
+    fadeInLeft
+    fadeInRight
+    fadeInDown
+    fadeInUp
+    zoomIn
+    zoomOut
+    flash
+}
