c0a4aa8a01d5dd0d1969c1a6c46900a252296cf6
Sergejs Tiselskis <slokas85@gmail.con>
2022-08-06 00:56:29 +0300

    version 2

---
+++ HW_3M_1L/Animation.swift
@@ -0,0 +1,23 @@
+//
+//  Animation.swift
+//  HW_3M_1L
+//
+//  Created by John Doe on 05/08/2022.
+//
+struct Animation {
+    
+    var name: String
+    var curve: String
+    var force: Float
+    var duration: Float
+    var delay: Float
+    
+    static func getNextAnimation() -> Animation {
+        Animation(name: AnimationTypes.shared.animationName.randomElement() ?? "shake",
+                  curve: AnimationTypes.shared.curveName.randomElement() ?? "spring",
+                  force: Float.random(in: 1...2),
+                  duration: Float.random(in: 1...3),
+                  delay: Float.random(in: 0.2...0.5)
+        )
+    }
+}
--- HW_3M_1L/MainViewController.swift
+++ HW_3M_1L/MainViewController.swift
@@ -6,18 +6,32 @@
 //
 
 import UIKit
+import SpringAnimation
 
 class MainViewController: UIViewController {
 
+    @IBOutlet var animationView: SpringView!
     
+    private var animation = Animation.getNextAnimation()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view.
+//    }
 
     
     @IBAction func animationButtonPressed(_ sender: UIButton) {
+        
+        print(animation.name, animation.force, animation.curve, animation.delay, animation.duration)
+        animationView.animation = animation.name
+        animationView.force = CGFloat(animation.force)
+        animationView.curve = animation.curve
+        animationView.delay = CGFloat(animation.delay)
+        animationView.duration = CGFloat(animation.duration)
+        animationView.animate()
+        
+        
     }
     
     
